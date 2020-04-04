23b759df402db017ef17b97a3082941b8b401753
Manjot G <kanwar.johal@reframeinc.com>
2020-04-04 15:09:18 -0400

    ?

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:rawnak88/test"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:rawnak88@localhost:5432/test"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # from models import Loading
